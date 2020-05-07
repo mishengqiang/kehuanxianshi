ec7b10059ceb1b6e4e3f7daf74f330d194714366
mishengqiang <13716526885@139.com>
2020-05-07 15:51:49 +0800

    :wrench: update: 修改网站配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,21 @@
 module.exports = {
-  base: "/kehuanxianshi/",
+  // base: "/kehuanxianshi/",
   title: "科幻现实",
   description: "科幻工作的技术分享 现实生活的人生思考",
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/homescreen144.png' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/homescreen144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
+    ['meta', { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge,chrome=1' }],
+    ['meta', { name: 'renderer', content: 'webkit' }],
+    ['meta', { name: 'force-rendering', content: 'webkit' }],
+    ['meta', { name: 'applicable-device', content: 'pc,mobile' }],
+  ],
   locales: { "/": { lang: "zh-CN" } },
   theme: "ououe",
   themeConfig: {
@@ -12,10 +26,10 @@ module.exports = {
       "/life/": "/images/life.jpg"
     },
     nav: [
-      { text: "主页", link: "/" },
+      { text: "首页", link: "/" },
       { text: "工作", link: "/work/" },
       { text: "生活", link: "/life/" },
-      { text: "关于", link: "/about/" }
+      { text: "关于", link: "/about/" },
     ],
     footer: [
       { text: "京ICP备15040086号", link: "http://www.beian.miit.gov.cn" }
@@ -26,19 +40,34 @@ module.exports = {
     },
     backgroundImage: false,
     // defaultTheme: { light: [6, 18], dark: [18, 6] },
-    showThemeButton: false
+    showThemeButton: false,
+    useVssue: true,
   },
   // 当你使用 defaultTheme 时，你需要增加一个如下的 postcss 插件
   postcss: {
     plugins: [
       require("css-prefers-color-scheme/postcss"),
+      require('postcss-propro'),
+      require('postcss-flex-alias'),
       require("autoprefixer")
     ]
   },
   plugins: [
-    ['@vuepress/last-updated', {
-      transformer: timestamp => {
-        return new Date(timestamp).toISOString()
+    ['@vssue/vuepress-plugin-vssue', {
+      platform: 'github',
+      owner: 'mishengqiang',
+      repo: 'kehuanxianshi',
+      clientId: 'd730c138b9e5aba08351',
+      clientSecret: '284228b35f01a6f81923651522312fa969d685b6'
+    }],
+    ['sitemap', {
+      hostname: 'http://www.kehuanxianshi.com/',
+      changefreq: 'weekly'
+    }],
+    ['@vuepress/pwa', {
+      updatePopup: {
+        message: '有新的文章发布',
+        buttonText: '刷新'
       }
     }]
   ]
